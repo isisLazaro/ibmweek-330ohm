773a544730bbdab0f6a2223d7ea2c937757d2093
OyukiCV <oyukivel@hotmail.com>
2019-08-03 15:04:38 -0500

    Form cotizador en html

---
+++ form.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+   var elems = document.querySelectorAll('select');
+   var instances = M.FormSelect.init(elems, options);
+ });
+ // Or with jQuery
+
+ $(document).ready(function(){
+   $('select').formSelect();
+ });
+ 
+ var instance = M.FormSelect.getInstance(elem);
+
+ instance.getSelectedValues();
